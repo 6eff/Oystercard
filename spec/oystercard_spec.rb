ec2ae900b3b396bd50f728044262f1dcbf94a654
6eff <6eff@mail.ru>
2016-07-14 17:08:36 +0300

    merge diff

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -2,55 +2,16 @@ require 'oystercard'
 
 describe Oystercard do
 min_fare = described_class.new.min_fare
-# let(:entry_station) {double('entry_station')}
-# let(:exit_station) {double('exit_station')}
-# let(:journey) {{:enter => entry_station, :exit => exit_station}}
 
   it 'shows card balance' do
     expect(subject.balance).to eq 0
   end
-
-  # it 'in_journey returns false initially' do
-  #   expect(subject).not_to be_in_journey
-  # end
-
-  # describe '#touch_in' do
-  #   before do
-  #     subject.top_up(min_fare)
-  #     subject.touch_in entry_station
-  #   end
-  #   it 'can touch_in' do
-  #     expect(subject).to be_in_journey
-  #   end
-  #   it 'stores entry station' do
-  #     expect(subject.journey[:enter]).to eq entry_station
-  #   end
-  # end
 
   context 'balance below minimum fare' do
     it 'raises error if balance is less than minimum fare' do
       expect{subject.touch_in double :entry_station}.to raise_error "balance too low"
     end
   end
-
-  # describe '#touch_out' do
-  #
-  #   before do
-  #     subject.top_up(min_fare)
-  #     subject.touch_in entry_station
-  #   end
-  #   it 'deducts the fare when we touch out' do
-  #     expect{subject.touch_out exit_station}.to change {subject.balance}.by -min_fare
-  #   end
-  #   it 'can touch out' do
-  #     subject.touch_out exit_station
-  #     expect(subject).not_to be_in_journey
-  #   end
-  #   it 'sets entry station to nil' do
-  #     subject.touch_out exit_station
-  #     expect(subject.journey[:enter]).to be_nil
-  #   end
-  # end
 
   describe "#top_up" do
     it 'tops up balance' do
@@ -69,16 +30,5 @@ min_fare = described_class.new.min_fare
       expect(subject.history).to be_empty
     end
   end
-
-=begin
- This is now a private method. No unit tests for private methods.
-
-  describe "#deduct" do
-    it 'deducts the fare amount from balance' do
-      subject.top_up(100)
-      expect{subject.deduct(50)}.to change {subject.balance}.by -50
-    end
-  end
-=end
 
 end
